4bb5fa2e718f3a6695c4383c1cb3637b2bf4ee55
seonyoung2.lee <seonyoung2.lee@lge.com>
2023-02-01 17:57:24 +0900

    add countdown and stopwatch
    :Release Notes:
    add countdown and stopwatch

--- gameBoard.js
+++ gameBoard.js
@@ -5,12 +5,19 @@ class GameBoard {
     //nums = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
     numberPhase1 = [];
     numberPhase2 = [];
-    
+    isCountDone = false;
+    min;
+    sec;
+    ms;
+    intervalId;
 
     constructor() {
         this.next = 1;
         this.makeNumberArray(this.numberPhase1, 1);
         this.makeNumberArray(this.numberPhase2, 26);
+        this.min = 0;
+        this.sec = 0;
+        this.ms = 0;
     }
 
     makeNumberArray(array, start) {
@@ -38,21 +45,24 @@ class GameBoard {
         document.getElementById("next").innerText = this.next;
     }
 
-    countAction(num) {
-        console.log(num);
+    countAction(count) {
+        console.log(count);
         //do action
-        if(num == 0) {
+        if(count === 0) {
             document.getElementById("count").innerText = "";
-            document.getElementById("page2").style.touchAction = "manipulation";
+            this.isCountDone = true;
+            this.startTimer();
+            //document.getElementById("page2").style.touchAction = "manipulation";
         }
         else
-            document.getElementById("count").innerText = num;
+            document.getElementById("count").innerText = count;
 
-        num--;
+        count--;
+
         //let self = this;
-        if(num >= 0)
-            setTimeout(()=> {this.countAction(num)}, 1000);
-            //setTimeout( function() { self.printAction(num); }, 1000);
+        if(count >= 0)
+            setTimeout(()=> {this.countAction(count)}, 1000);
+            //setTimeout( function() { self.printAction(count); }, 1000);
     }
 
     startCount() {  // 3 2 1
@@ -73,7 +83,43 @@ class GameBoard {
         }, 1000);*/
     }
 
+    timerAction() {
+        //next
+        this.ms++;
+        if(this.ms === 10) {
+            this.sec++;
+            this.ms = 0;
+        }
 
+        if(this.sec === 60) {
+            this.min++;
+            this.sec = 0;
+        }
+
+        let pMin = (this.min >= 10) ? this.min : "0"+this.min;
+        let pSec = (this.sec >= 10) ? this.sec : "0"+this.sec;
+        let pMs = this.ms;
+
+        //print
+        document.getElementById("timeScore").innerText = pMin+":"+pSec+":"+pMs;
+        //console.log(document.getElementById("timeScore").innerHTML);
+
+
+    }
+
+    startTimer() {
+        setInterval(()=> {this.timerAction()}, 100);
+
+    }
+
+    gameClear() {
+        console.log("Great!");
+        document.getElementById("count").innerText = document.getElementById("timeScore").innerHTML;
+
+        //clearTimeout();
+        //clearInterval(intervalId);
+
+    }
     addTouchEvent() { 
         /*for(let row = 0; row < 5; row++) {
             for(let col = 0; col < 5; col++) {
@@ -88,15 +134,23 @@ class GameBoard {
     }
 
     tapAction(evt) {
+        if(this.isCountDone === false) {
+            console.log("counting..");
+            return;
+        }
         let element = document.getElementById(evt.target.id);
         console.log("target:"+evt.target.id+" text:"+element.innerHTML);
         if(this.next === Number(element.innerHTML)) {
             console.log("!!");
+            if(this.next === 50) {
+                this.gameClear();
+            }
+
             if(this.next <= 25) {
                 element.innerText = this.numberPhase2[this.next-1];
             }
             else {
-                element.innerText = "";
+                element.innerText = " ";
                 element.style.backgroundColor = 'rgb(32 ,30 ,30)';
             }
             this.next++;
